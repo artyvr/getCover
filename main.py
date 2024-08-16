7af8e51ab9bc6dcd3e9673a4738aa7f8e2227ecd
arty <artyvr@yandex.ru>
2024-08-16 23:39:51 +0300

    Lint

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
+""" Get cover """
 
 import os
-import json
-import wget
 import glob
 import logging
-import requests
 import mimetypes
 from pathlib import Path
-from mutagen.mp3 import MP3
 from datetime import datetime
 from multiprocessing import Pool
-from dotenv import dotenv_values
-from mutagen.id3 import ID3, APIC
 from urllib.parse import urlsplit
+from mutagen.id3 import ID3, APIC
+from mutagen.mp3 import MP3
+import wget
+import requests
+from dotenv import dotenv_values
 
 
 now = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
@@ -29,10 +29,11 @@ threads = int(config['THREADS'])
 logs_dir = config['LOGS_DIR']
 
 logging.basicConfig(level=logging.INFO, filename=f"{logs_dir}/getCover_{now}.log", filemode="w")
-logging.info(f"Start at {now} with {threads} threads")
+logging.info("Start at %s with %s threads", now, threads)
 
 
-def lastFMRequest(artist, album):
+def last_fm_request(artist, album):
+    """ Request to lastfm API """
     try:
         headers = {
             'user-agent': 'Dataquest'
@@ -44,47 +45,55 @@ def lastFMRequest(artist, album):
             'artist': artist,
             'album': album
         }
-        response = requests.get(api_url, headers=headers, params=payload)
-        logging.info(f"Get info for {artist} - {album} - OK")
+        response = requests.get(api_url, headers=headers, params=payload, timeout=10)
+        logging.info("Get info for %s - %s - OK", artist, album)
         return response
-    except Exception as e:
-        logging.error(f"{artist} - {album}, LastFM API error: {e}")
+    except requests.exceptions.ConnectionError:
+        logging.error("%s - %s LastFM API error - A connection error occurred." , artist, album)
+    except requests.exceptions.Timeout:
+        logging.error("%s - %s LastFM API error - The request timed out." , artist, album)
+    except requests.exceptions.HTTPError as e:
+        logging.error("%s - %s LastFM API error - HTTP Error: %s", artist, album, e)
+    except requests.exceptions.RequestException as e:
+        logging.error("%s - %s LastFM API error - An error occurred: %s", artist, album, e)
 
 
-def getMP3Img(file):
+def get_mp3_img(file):
+    """ Download album image """
     try:
         file_ = MP3(file)
         album = file_.tags['TALB']
         artist = file_.tags['TPE1']
         if album is not None and artist is not None:
-            responseData = lastFMRequest(artist, album)
-            img_url = responseData.json()['album']['image'][5]['#text']
-            album = responseData.json()['album']['name']
-            imgFilePath = f"{os.path.dirname(file)}/{artist}-{album}_cover{os.path.splitext(urlsplit(img_url).path)[-1]}"
-            if not Path(imgFilePath).exists():
-                wget.download(img_url, imgFilePath)
-                logging.info(f"Download image for {file} - OK")
+            response_data = last_fm_request(artist, album)
+            img_url = response_data.json()['album']['image'][5]['#text']
+            album = response_data.json()['album']['name']
+            img_file_path = f"{os.path.dirname(file)}/{artist}-{album}_cover{os.path.splitext(urlsplit(img_url).path)[-1]}"
+            if not Path(img_file_path).exists():
+                wget.download(img_url, img_file_path)
+                logging.info("Download image for %s - OK", file)
             else:
-                logging.info(f"Image for {file} already exist")
-            return imgFilePath
+                logging.info("Image for %s already exist", file)
+            return img_file_path
         else:
-            logging.warning(f"{file} - Album or Artist is empty, skip file")
+            logging.warning("%s - Album or Artist is empty, skip file", file)
             return None
     except Exception as e:
-        logging.error(f"{file} - Get image error: {e}, skip file")
+        logging.error("%s - Get image error: %e, skip file", file, e)
 
 
 
-def addImgToMp3(file):
+def add_img_to_mp3(file):
+    """ Add image to mp3 file """
     try:
         file_ = MP3(file, ID3=ID3)
-        img = getMP3Img(file)
+        img = get_mp3_img(file)
         if img is not None:
             file_.tags.add(APIC(
-                    encoding=0, 
-                    mime=mimetypes.guess_type(img), 
-                    type=3, 
-                    desc='Cover (front)', 
+                    encoding=0,
+                    mime=mimetypes.guess_type(img),
+                    type=3,
+                    desc='Cover (front)',
                     data=open(img,'rb').read()
                     ))
             file_.save()
@@ -97,15 +106,16 @@ def addImgToMp3(file):
 
 
 def main():
+    """ Main """
     start = datetime.now()
     files_ = glob.glob(dirPath + '/**/*.mp3', recursive=True)
     #for file_ in files_:
     #    addImgToMp3(file_)
     with Pool(threads) as p:
-        p.map(addImgToMp3, files_)
+        p.map(add_img_to_mp3, files_)
     end = datetime.now()
     total = str(end - start)
-    logging.info(f"All done for {total}. ")
+    logging.info("All done for %s. ", total)
 
 
 if __name__ == '__main__':
