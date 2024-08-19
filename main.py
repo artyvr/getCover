9675b61fed1a131cb496d15e988f8ddbffb773c1
arty <artyvr@yandex.ru>
2024-08-19 21:32:19 +0300

    Update exceptions

--- main.py
+++ main.py
@@ -35,10 +35,14 @@ logging.info("Start at %s with %s threads", now, threads)
 def download_file(url, filename):
     """ Download file from over http """
     try:
-        with open(filename, 'wb') as file:
-            file.write(requests.get(url, timeout=10).content)
-    except Exception as e:
+        with open(filename, 'wb') as f:
+            f.write(requests.get(url, timeout=10).content)
+        return True
+    except OSError as e:
         logging.error("%s - %s Download file error %s", url, filename, e)
+        return False
+    finally:
+        f.close()
 
 
 def last_fm_request(artist, album):
@@ -94,17 +98,15 @@ def get_mp3_img(file):
             if not Path(img_file_path).exists():
                 download_file(img_url, img_file_path)
                 logging.info("Download image for %s - OK", file)
-            else:
-                logging.info("Image for %s already exist", file)
-            return img_file_path
-        else:
-            logging.warning("%s - Album or Artist is empty, skip file", file)
-            return None
+                return img_file_path
+            logging.info("Image for %s already exist", file)
+        logging.warning("%s - Album or Artist is empty, skip file", file)
+        return None
     except MutagenError as e:
-        logging.error("%s - Mutagen error: %e, skip file", file, e)
+        logging.error("%s - Mutagen error: %s, skip file", file, e)
         return None
     except Exception as e:
-        logging.error("%s - Get image error: %e, skip file", file, e)
+        logging.error("%s - Get image error: %s, skip file", file, e)
         return None
 
 
@@ -114,23 +116,25 @@ def add_img_to_mp3(file):
     try:
         file_ = MP3(file, ID3=ID3)
         img = get_mp3_img(file)
+        with open(img,'rb') as f:
+            file_data = f.read()
         if img is not None:
             file_.tags.add(APIC(
                     encoding=0,
                     mime=mimetypes.guess_type(img),
                     type=3,
                     desc='Cover (front)',
-                    data=open(img,'rb').read()
+                    data=file_data
                     ))
             file_.save()
             logging.info("Add image for %s - OK", file)
-        else:
-            logging.warning("%s - Image not found, skip file", file)
-            return None
+        logging.warning("%s - Image not found, skip file", file)
     except MutagenError as e:
-        logging.error("%s - Mutagen error: %e, skip file", file, e)
+        logging.error("%s - Mutagen error: %s, skip file", file, e)
     except Exception as e:
         logging.error("%s - Add image to mp3 file error: %s, skip file", file, e)
+    finally:
+        f.close()
 
 
 def main():
